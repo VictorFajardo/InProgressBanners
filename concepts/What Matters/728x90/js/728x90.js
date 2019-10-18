dcb66a95b4c4f66d95f2a2022c17993658f0bf7a
Victor Fajardo <fajardo.de.leon@gmail.com>
2019-10-17 22:45:33 -0400

    Thursday

---
+++ concepts/What Matters/728x90/js/728x90.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+
+  // Definitions
+  // var clickarea = document.getElementById("click-area");
+
+  // Functions
+
+	// cta.addEventListener ("click", function(e) { e.preventDefault(); e.stopPropagation(); Enabler.exit("clickTag1"); }, true);
+	// clickarea.addEventListener("click", function(e) { e.preventDefault(); e.stopPropagation(); Enabler.exit("clickTag1"); }, true);
+
+  // Animations
+
+  TweenLite.to(img1, .5, {opacity: 1, ease: Power3.easeOut, delay: .5}, 0);
+  TweenLite.to(text1a, .5, {opacity: 1, x: 6, ease: Power2.easeOut, delay: 1}, 0);
+  TweenLite.to(text1b, .5, {opacity: 1, x: 6, ease: Power2.easeOut, delay: 1.25}, 0);
+  TweenLite.to(yes, .5, {opacity: 1, ease: Power2.easeOut, delay: 1.75}, 0);
+  TweenLite.to(no, .5, {opacity: 1, ease: Power2.easeOut, delay: 2}, 0);
+  TweenLite.to(yes, .25, {scale: 1.2, className: "shadow", ease: Power2.easeOut, delay: 2.25}, 0);
+  
+  TweenLite.to([text1a, text1b, yes, no], .75, {opacity: 0, ease: Power3.easeOut, delay: 3.5}, 0); 
+  TweenLite.to([bg1, img1, img2, pylon], 1.5, {x: -67, ease: Power2.easeOut, delay: 3.5}, 0);
+  TweenLite.to(text2a, .5, {opacity: 1, x: 6, ease: Power2.easeOut, delay: 3.5}, 0);
+  TweenLite.to(text2b, .5, {opacity: 1, x: 6, ease: Power2.easeOut, delay: 3.75}, 0);
+  TweenLite.to(img2, .5, {opacity: 1, ease: Power3.easeOut, delay: 4.5}, 0);
+  //
+  TweenLite.set(img1, {opacity: 0, delay: 6}, 0);
+  TweenLite.to(img2, .75, {opacity: 0, ease: Power3.easeOut, delay: 6}, 0);
+  TweenLite.to([text2a, text2b], .75, {opacity: 0, ease: Power3.easeOut, delay: 6}, 0); 
+  TweenLite.to(img3, .75, {opacity: 1, ease: Power3.easeOut, delay: 6}, 0);
+  TweenLite.to(text3, .75, {opacity: 1, x: 6, ease: Power3.easeOut, delay: 6}, 0);
+
+  TweenLite.to(uc, .25, {y: 11, ease: Power2.easeOut, delay: 8.5}, 0);
+  TweenLite.to([img3, pylon], .5, {opacity: 0, ease: Power2.easeOut, delay: 8.5}, 0);
+  TweenLite.to(bg1, 1.2, {x: -120, y: 188, ease: Power2.easeOut, delay: 8.5}, 0);
+  TweenLite.to([img3, pylon], 1, {y: 172, ease: Power2.easeOut, delay: 8.5}, 0);
+  TweenLite.to([white, text3], .25, {x: -356, ease: Power2.easeOut, delay: 8.5}, 0);
+  // TweenLite.to(isi, .25, {y: -118, ease: Power2.easeOut, delay: 8.5}, 0);
+  TweenLite.to([text4, xeljanz, available], .5, {opacity: 1, ease: Power2.easeOut, delay: 9.5}, 0);
+
+  TweenLite.to(text4, .5, {opacity: 0, ease: Power2.easeOut, delay: 14}, 0);
+  TweenLite.to(text5, .5, {opacity: 1, ease: Power2.easeOut, delay: 14.25}, 0);
+  TweenLite.to(cta, .5, {opacity: 1, ease: Power2.easeOut, delay: 14.5}, 0);
+
+}//end
